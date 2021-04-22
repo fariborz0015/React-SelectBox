6184ab6bbda4602f7fd1051e0c4146c5fbe626b3
fariborz shalghooni <fariborz0015@gmail.com>
2021-04-22 18:15:02 +0430

    change readme

--- src/SelectBox.js
+++ src/SelectBox.js
@@ -25,72 +25,73 @@ function SelectBox(props) {
 
   return (
     <>
-
-      <div className="select-box"   >
-        <div className="select-placeholder" onClick={e => openSet(!open)} >
-          <span>  {selectedOption.item_label === null ? " یک گزینه انتخاب کنید " : selectedOption.item_label}  </span>
-          <span className={`arrow-down ${!open ? 'active' : ''}`}><img src="./arrow-down.svg" alt="" /></span>
-          <span className={`arrow-up ${!open ? '' : 'active'}`}><img src="./arrow-down.svg" alt="" /></span>
-        </div>
-        <input
-          type="text"
-          onClick={e => openSet(true)}
-          className={`select-search ${props.searchable == true && open == true ? 'active' : ''}`}
-          onChange={e => props.searching(e.target.value)}
-          onChange={e => searchingSet(e.target.value)}
-          placeholder="جستجو کنید"
-          value={searching}
-        />
-        <div className={`options-box ${open ? 'active' : ''}`}>
-          <div className="option disabled"  >
-            {props.placeholderOption ? props.placeholderOption : ' یک گزینه انتخاب کنید'}
+      <div className="w-100" >
+        <div className="container">
+          <div className="select-box"   >
+            <div className="select-placeholder" onClick={e => openSet(!open)} >
+              <span>  {selectedOption.item_label === null ? " یک گزینه انتخاب کنید " : selectedOption.item_label}  </span>
+              <span className={`arrow-down ${!open ? 'active' : ''}`}><img src="./arrow-down.svg" alt="" /></span>
+              <span className={`arrow-up ${!open ? '' : 'active'}`}><img src="./arrow-down.svg" alt="" /></span>
+            </div>
+            <input
+              type="text"
+              onClick={e => openSet(true)}
+              className={`select-search ${props.searchable == true && open == true ? 'active' : ''}`}
+              onChange={e => props.searching(e.target.value)}
+              onChange={e => searchingSet(e.target.value)}
+              placeholder="جستجو کنید"
+              value={searching}
+            />
+            <div className={`options-box ${open ? 'active' : ''}`}>
+              <div className="option disabled"  >
+                {props.placeholderOption ? props.placeholderOption : ' یک گزینه انتخاب کنید'}
+              </div>
+              {
+                props.options.map(item => {
+                  return (
+                    props.searchable ?
+                      item.label.match(searching) ?
+                        <div
+                          className={`option ${item.disabled ? 'disabled' : ''}`}
+                          key={`option_${item.value}`}
+                          onClick={e => !item.disabled ? selectOption(e) : null}
+                          data-label={item.label}
+                          data-value={item.value}>
+                          {item.label}
+                        </div>
+                        :
+                        null
+                      :
+                      <div
+                        className={`option ${item.disabled ? 'disabled' : ''}`}
+                        key={`option_${item.value}`}
+                        onClick={e => !item.disabled ? selectOption(e) : null}
+                        data-label={item.label}
+                        data-value={item.value}>
+                        {item.label}
+                      </div>
+                  )
+                })
+              }
+            </div>
           </div>
           {
-            props.options.map(item => {
-              return (
-                props.searchable ?
-                  item.label.match(searching) ?
-                    <div
-                      className={`option ${item.disabled ? 'disabled' : ''}`}
-                      key={`option_${item.value}`}
-                      onClick={e => !item.disabled ? selectOption(e) : null}
-                      data-label={item.label}
-                      data-value={item.value}>
-                      {item.label}
-                    </div>
-                    :
-                    null
-                  :
-                  <div
-                    className={`option ${item.disabled ? 'disabled' : ''}`}
-                    key={`option_${item.value}`}
-                    onClick={e => !item.disabled ? selectOption(e) : null}
-                    data-label={item.label}
-                    data-value={item.value}>
-                    {item.label}
-                  </div>
-              )
-            })
+            props.resetable ?
+              selectedOption.item_label && selectedOption.item_value !== null ?
+                <button
+                  className="select-box-reset"
+                  onClick={e => selectedOptionSet({
+                    item_label: null,
+                    item_value: null,
+                    item_key: null
+                  })}
+                >&times;</button>
+                : null
+              :
+              null
           }
         </div>
       </div>
-      {
-        props.resetable ?
-          selectedOption.item_label && selectedOption.item_value !== null ?
-            <button
-              className="select-box-reset"
-              onClick={e => selectedOptionSet({
-                item_label: null,
-                item_value: null,
-                item_key: null
-              })}
-            >&times;</button>
-            : null
-          :
-          null
-      }
-
-
       <select className="hiddenSelectbox" name={props.name} ref={props.ref} id={props.id}>
         {
           selectedOption.item_label && selectedOption.item_value !== null ?
